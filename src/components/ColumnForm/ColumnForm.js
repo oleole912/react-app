b28a334522203bb35edb99ddd6579ebc4d40d8d5
Aleksandra Bukalskai <bukalska.aleksandra@gmail.com>
2022-11-12 22:58:33 +0000

    Add ColumnForm component

---
+++ src/components/ColumnForm/ColumnForm.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import Button from "../Button/Button";
+import TextInput from "../TextInput/TextInput";
+import styles from "./ColumnForm.module.scss";
+
+const ColumnForm = (props) => {
+  const [title, setTitle] = useState("");
+  const [icon, setIcon] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.action({ title: title, icon: icon });
+    setTitle("");
+    setIcon("");
+  };
+
+  return (
+    <form className={styles.columnForm} onSubmit={handleSubmit}>
+      <span className={styles.label}>Title:</span>
+      <TextInput value={title} onChange={(e) => setTitle(e.target.value)} />
+      <span className={styles.label}>Icon:</span>
+      <TextInput value={icon} onChange={(e) => setIcon(e.target.value)} />
+      <Button>Add column</Button>
+    </form>
+  );
+};
+
+export default ColumnForm;
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,20 +1,40 @@
-import styles from './List.module.scss';
-import Column from '../Column/Column';
+import { useState } from "react";
+import shortid from 'shortid';
+
+import styles from "./List.module.scss";
+import Column from "../Column/Column";
+import ColumnForm from "../ColumnForm/ColumnForm";
+
 
 const List = () => {
-    return (
-        <div className={styles.list}>
-            <header className={styles.header}>
-                <h1 className={styles.title}>Things to do <span>soon</span></h1>
-                <p className={styles.description}>Interesting things I want to check out</p>
-            </header>
-            <section className={styles.columns}>
-                <Column title="Books" icon="book" />
-                <Column title="Movies" icon="gamepad" />
-                <Column title="Games" icon="film" />
-            </section>
-        </div>
-    )
-}
+  const [columns, setColumns] = useState([
+    { id: 1, title: "Books", icon: "book" },
+    { id: 2, title: "Movies", icon: "film" },
+    { id: 3, title: "Games", icon: "gamepad" },
+  ]);
+
+  const addColumn = newColumn => {
+    setColumns([...columns, {id: shortid(), title: newColumn.title, icon: newColumn.icon}]);
+  };
+
+  return (
+    <div className={styles.list}>
+      <header className={styles.header}>
+        <h1 className={styles.title}>
+          Things to do <span>soon</span>
+        </h1>
+        <p className={styles.description}>
+          Interesting things I want to check out
+        </p>
+      </header>
+      <section className={styles.columns}>
+        {columns.map((column) => (
+          <Column key={column.id} title={column.title} icon={column.icon} />
+        ))}
+      </section>
+      <ColumnForm action={addColumn} />
+    </div>
+  )
+};
 
 export default List;
