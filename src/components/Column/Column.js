e3b47a00e614ce1ecfedc2b7ae00d83f6e70cd96
Aleksandra Bukalskai <bukalska.aleksandra@gmail.com>
2022-11-23 10:44:59 +0000

    Use Redux for storing data

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,14 +1,16 @@
 import styles from './Column.module.scss';
 import Card from '../Card/Card';
 import CardForm from '../CardForm/CardForm';
+import { useSelector } from 'react-redux';
 
-const Column = props => {
+const Column = (props) => {
+    const cards = useSelector(state => state.cards.filter(card => card.columnId === props.id));
 
     return (
         <article className={styles.column}>
             <h1 className={styles.title}><span className={styles.icon + ' fa fa-' + props.icon}></span>{props.title}</h1>
             <ul className={styles.cards}>
-                {props.cards.map(card => <Card title={card.title} key={card.id} />)}
+                {cards.map(card => <Card title={card.title} key={card.id} />)}
             </ul>
             <CardForm columnId={props.id} action={props.action} />
         </article>
